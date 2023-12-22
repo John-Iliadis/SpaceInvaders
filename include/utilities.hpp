c97fd1ac3314f1eedda72a3d713a778ec3f705b3
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2023-12-22 18:24:31 +1100

    Added some utility functions

---
+++ include/utilities.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Gianni on 18/12/2023.
+//
+
+#ifndef SPACEINVADERS_UTILITIES_HPP
+#define SPACEINVADERS_UTILITIES_HPP
+
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/Shape.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+
+void center_text(sf::Text& text);
+void center_shape(sf::Shape& shape);
+void center_sprite(sf::Sprite& sprite);
+
+std::string key_to_string(sf::Keyboard::Key key);
+
+#endif //SPACEINVADERS_UTILITIES_HPP
