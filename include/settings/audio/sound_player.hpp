1727126646c0924d3c63074137633047831da842
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2023-12-22 18:25:20 +1100

    Moved to \sounds

--- include/settings/audio/sound_player.hpp
+++ include/settings/audio/sound_player.hpp
@@ -6,24 +6,36 @@
 #define SPACEINVADERS_SOUND_PLAYER_HPP
 
 #include <list>
+#include <string>
+#include <fstream>
+#include <iomanip>
 
 #include <SFML/Audio/Sound.hpp>
 
-#include "asset_manager.hpp"
-
+#include "../../asset_manager.hpp"
+#include "../../json.hpp"
 
 
 class SoundPlayer
 {
 public:
+    static void init();
+    static void save();
+
     static void play(const std::string& key);
     static void stop();
+
+    static void increase_volume();
+    static void decrease_volume();
+    static void set_volume(int volume);
+    static int get_volume();
 
 private:
     SoundPlayer() = default;
 
 private:
     static std::list<sf::Sound> sounds;
+    static json config;
 };
 
 
