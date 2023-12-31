171e21b1fe74c35cb64f3cf99fa9646e3ccc59be
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2023-12-31 16:25:28 +1100

    Added the pause state

---
+++ include/game_states/pause_state.hpp
@@ -0,0 +1,32 @@
+//
+// Created by Gianni on 31/12/2023.
+//
+
+#ifndef SPACEINVADERS_PAUSE_STATE_HPP
+#define SPACEINVADERS_PAUSE_STATE_HPP
+
+
+#include <SFML/Graphics/RectangleShape.hpp>
+#include "../GUI/gui_includes.hpp"
+#include "state.hpp"
+
+
+class PauseState : public State
+{
+public:
+    PauseState(StateStack& state_stack, sf::RenderWindow& window);
+
+    void render() override;
+    bool update() override;
+    bool handle_event(const sf::Event &event) override;
+
+private:
+    void setup_gui(sf::Vector2u window_size);
+
+private:
+    sf::RectangleShape overlay;
+    GUI_Container gui_container;
+};
+
+
+#endif //SPACEINVADERS_PAUSE_STATE_HPP
---
+++ src/game_states/pause_state.cpp
@@ -0,0 +1,92 @@
+//
+// Created by Gianni on 31/12/2023.
+//
+
+#include "../../include/game_states/pause_state.hpp"
+
+PauseState::PauseState(StateStack &state_stack, sf::RenderWindow &window)
+        : State(state_stack, window)
+{
+    auto window_size = window.getSize();
+
+    overlay.setSize({(float)window_size.x, (float)window_size.y});
+    overlay.setFillColor({0, 0, 0, 128});
+
+    setup_gui(window_size);
+    gui_container.select_first();
+}
+
+void PauseState::render()
+{
+    window.draw(overlay);
+
+    if (is_current)
+    {
+        window.draw(gui_container);
+    }
+}
+
+bool PauseState::update()
+{
+    return false;
+}
+
+bool PauseState::handle_event(const sf::Event &event)
+{
+    gui_container.handle_event(event);
+    return false;
+}
+
+void PauseState::setup_gui(sf::Vector2u window_size)
+{
+    GUI_Builder builder;
+
+    auto resume = builder.set_text("RESUME")
+            .set_position(window_size.x / 2.f, 300)
+            .set_callback_1([this] () {
+                request_stack_pop();
+            })
+            .make_button();
+
+    builder.clear();
+
+    // TODO: Implement after making the game state
+    auto restart = builder.set_text("RESTART")
+            .set_position(window_size.x / 2.f, 400)
+            .set_callback_1(nullptr)
+            .make_button();
+
+    builder.clear();
+
+    auto settings = builder.set_text("SETTINGS")
+            .set_position(window_size.x / 2.f, 500)
+            .set_callback_1([this] () {
+                request_stack_push(StateID::SETTINGS);
+            })
+            .make_button();
+
+    builder.clear();
+
+    auto quit_to_menu = builder.set_text("QUIT TO MENU")
+            .set_position(window_size.x / 2.f, 600)
+            .set_callback_1([this] () {
+                request_state_clear();
+                request_stack_push(StateID::MENU);
+            })
+            .make_button();
+
+    builder.clear();
+
+    auto quit_to_desktop = builder.set_text("QUIT TO DESKTOP")
+            .set_position(window_size.x / 2.f, 700)
+            .set_callback_1([this] () {window.close();})
+            .make_button();
+
+    builder.clear();
+
+    gui_container.pack(resume);
+    gui_container.pack(restart);
+    gui_container.pack(settings);
+    gui_container.pack(quit_to_menu);
+    gui_container.pack(quit_to_desktop);
+}
